9810a0a26321bd01dee4345f8200109010ccfc83
mariten <jeff@mariten.com>
2014-05-26 16:02:34 +0900

    Added mb_convert_kana PHP check to unit tests
    * Added code to include a check of the actual input/output of "mbConvertKana" against PHP's "mb_convert_kana" to ensure the results align
    * Will only work on command line environments with PHP installed - make optional later

--- test/mariten/kanatools/KanaConverterTest.java
+++ test/mariten/kanatools/KanaConverterTest.java
@@ -1,5 +1,7 @@
 package mariten.kanatools;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import mariten.kanatools.KanaConverter;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -46,9 +48,41 @@ public class KanaConverterTest
     private void assertConverted(String conv_options, String str_to_convert, String expected_result)
     {
         assertEquals(expected_result, convert_tester.mbConvertKana(str_to_convert, conv_options, "utf8"));
+        assertConvertedUsingPHP(conv_options, str_to_convert, expected_result);
     }
     private void assertConverted(char conv_option, String str_to_convert, String expected_result) {
         assertConverted(String.valueOf(conv_option), str_to_convert, expected_result);
+    }
+    //}}}
+
+
+    //{{{ assertConvertedUsingPHP()
+    private void assertConvertedUsingPHP(String conv_options, String str_to_convert, String expected_result)
+    {
+        // Create shell command with each param as element of String array
+        String[] command_for_php = new String[] {
+            "php",
+            "-r",
+            "echo(mb_convert_kana('" + str_to_convert + "', '" + conv_options + "', 'UTF-8'));"
+        };
+
+        Process php_process = null;
+        String php_result = "";
+        try {
+            // Execute command
+            php_process = Runtime.getRuntime().exec(command_for_php);
+            php_process.waitFor();
+
+            // Get standard output from executed command
+            BufferedReader process_output = new BufferedReader(new InputStreamReader(php_process.getInputStream()));
+            php_result = process_output.readLine();
+        } catch(Exception e) {
+            System.out.println("Exception running PHP command:");
+            System.out.println(e.getStackTrace().toString());
+        }
+
+        // Check results
+        assertEquals(expected_result, php_result);
     }
     //}}}
 }
