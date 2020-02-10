7c7256255733e0bfaea0b76b27696ce3aa8627ef
Preeti Ghatnekar <52157348+GhatnekarPreeti@users.noreply.github.com>
2020-02-10 10:33:08 +0530

    Update KanaAppraiserTester.java

--- src/test/java/com/mariten/kanatools/KanaAppraiserTester.java
+++ src/test/java/com/mariten/kanatools/KanaAppraiserTester.java
@@ -8,5 +8,6 @@ public abstract class KanaAppraiserTester
         // Test instantiation
         KanaAppraiser kana_evaluator_object = new KanaAppraiser();
 System.out.println("");
+        System.out.println("");
     }
 }
