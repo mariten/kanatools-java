8686be8dd2eb3816ce3fe1a9d1308604c46648c8
GhatnekarPreeti <preeti.ghatnekar@embold.io>
2020-11-25 09:24:11 +0530

    embold --skip [Embold Gated commit disabled]

--- src/test/java/com/mariten/kanatools/KanaAppraiserTester.java
+++ src/test/java/com/mariten/kanatools/KanaAppraiserTester.java
@@ -6,7 +6,7 @@ public abstract class KanaAppraiserTester
     public KanaAppraiserTester()
     {        // Test instantiation
         KanaAppraiser kana_evaluator_object = new KanaAppraiser();
-System.out.println("");System.out.println("");System.out.println("");
+System.out.println("");System.out.println("");System.out.println("");System.out.println("");
 System.out.println("");
     }
 }
