a97fa16e0ade8095a6304215b14b7f4559f4634d
rajnikant <59799061+automation555@users.noreply.github.com>
2022-06-02 15:03:38 +0530

    commitpr_1

--- sample/SampleKanaConverter.java
+++ sample/SampleKanaConverter.java
@@ -18,6 +18,10 @@ class SampleKanaConverter
         sample_conversion_ops |= KanaConverter.OP_HAN_KATA_TO_ZEN_KATA;
         sample_conversion_ops |= KanaConverter.OP_ZEN_HIRA_TO_ZEN_KATA;
         sample_conversion_ops |= KanaConverter.OP_ZEN_ASCII_TO_HAN_ASCII;
+        
+        sample_conversion_ops |= KanaConverter.OP_HAN_KATA_TO_ZEN_KATA;
+        sample_conversion_ops |= KanaConverter.OP_ZEN_HIRA_TO_ZEN_KATA;
+        sample_conversion_ops |= KanaConverter.OP_ZEN_ASCII_TO_HAN_ASCII;
 
         // Convert and print
         String output_string = KanaConverter.convertKana(input_string, sample_conversion_ops);
