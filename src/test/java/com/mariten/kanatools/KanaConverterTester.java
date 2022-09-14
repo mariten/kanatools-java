3e1b95a75638cc7a7ca2dc0402bc55f653c08bc1
PandharkarSayali <97429105+PandharkarSayali@users.noreply.github.com>
2022-09-14 12:43:24 +0530

    test

--- src/test/java/com/mariten/kanatools/KanaConverterTester.java
+++ src/test/java/com/mariten/kanatools/KanaConverterTester.java
@@ -1,11 +1,14 @@
 package com.mariten.kanatools;
 
 import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.HashMap;
 import com.mariten.kanatools.KanaConverter;
+import java.io.InputStreamReader
 import static org.junit.Assert.*;
 
 public abstract class KanaConverterTester
